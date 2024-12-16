48d2aba66893623f6e27c11404e4dd5b060082b1
Teemu Taivainen <teemu.taivainen@hotmail.fi>
2024-12-16 20:21:41 +0200

    Day 5 part 2 done

--- src/days/day5.rs
+++ src/days/day5.rs
@@ -3,71 +3,84 @@ use std::collections::{HashMap, HashSet};
 use crate::utils::parse_str_slice_to_integers;
 
 pub fn solve() -> (u32, u32) {
-
-    let (must_precede, page_updates) = parse_input();
+    // Get info about which numbers come after a certain number and all the updates.
+    let (successors, page_updates) = parse_input();
 
     let part1_result: u32 = page_updates.iter()
-        .filter(|update| is_in_correct_order(update, &must_precede))
+        .filter(|update| is_in_correct_order(update, &successors))
         .map(|update| update[update.len() / 2])
         .sum::<u32>();
 
-    let incorrectly_ordered: Vec<Vec<u32>> = page_updates.iter()
-        .filter(|update| !is_in_correct_order(update, &must_precede))
-        .cloned()
-        .collect();
-
+    let part2_result: u32 = page_updates.iter()
+        .filter(|update| !is_in_correct_order(update, &successors))
+        .map(|incorrect_update| fix_order(incorrect_update, &successors))
+        .map(|corrected_update| corrected_update[corrected_update.len() / 2])
+        .sum::<u32>();
     
-
-    
-    (part1_result, 0)
+    (part1_result, part2_result)
 }
 
-fn is_in_correct_order(page_update: &Vec<u32>, must_precede: &HashMap<u32, HashSet<u32>>) -> bool {
+/// Check if update is in correct order.
+fn is_in_correct_order(page_update: &Vec<u32>, successors: &HashMap<u32, HashSet<u32>>) -> bool {
 
     for (page_idx, page_num) in page_update.iter().enumerate() {
-
-        for prev_page in page_update[..page_idx].iter() {
-            if let Some(predecessors) = must_precede.get(&prev_page) {
-                // There are number(s) that must be before the previous page.
-                // Check that the current one is not one of them.
-                if predecessors.contains(page_num) {
+        if let Some(successor_list) = successors.get(page_num) {
+            // The current page number has numbers that should be after it.
+            // Check that no such number has occurred before it.
+            for predecessor in page_update[..page_idx].iter() {
+                if successor_list.contains(predecessor) {
                     return false;
                 }
             }
-        }   
+        }
     }
     return true;
 }
 
-/*
-fn fix_order(page_update: &Vec<u32>, must_precede: &HashMap<u32, HashSet<u32>>) -> Vec<u32> {
+/// Return a new update that is the original but correctly ordered
+fn fix_order(page_update: &Vec<u32>, successors: &HashMap<u32, HashSet<u32>>) -> Vec<u32> {
+    // Use dfs to topologically sort the update.
+    let mut dfs_visited_order = subgraph_dfs(successors, page_update);
+    dfs_visited_order.reverse();
+    dfs_visited_order
+}
 
-    let mut corrected_update = page_update.clone();
+// Perform dfs for the subgraph consisting only some nodes (nodes_to_consider)
+fn subgraph_dfs(adj_list: &HashMap<u32, HashSet<u32>>, nodes_to_consider: &[u32]) -> Vec<u32> {
 
-    for (page_idx, page_num) in page_update.iter().enumerate() {
+    let mut visited: HashSet<u32> = HashSet::new();
+    let mut finished_order: Vec<u32> = Vec::new();
 
-        for prev_page in pages_in_update {
-            if let Some(predecessors) = must_precede.get(&prev_page) {
-                // There are number(s) that must be before the previous page.
-                // Check if the current one is one of them.
-                if predecessors.contains(page_num) {
-                    
+    fn dfs(current_node: u32, adj_list: &HashMap<u32, HashSet<u32>>, nodes_to_consider: &[u32],
+        visited: &mut HashSet<u32>, finished_order: &mut Vec<u32>) {
+        
+        visited.insert(current_node);
+
+        let neighbors = adj_list.get(&current_node);
+
+        if let Some(neighbor_list) = neighbors {
+            for &neighbor in neighbor_list {
+                if nodes_to_consider.contains(&neighbor) && !visited.contains(&neighbor) {
+                    dfs(neighbor, adj_list, nodes_to_consider, visited, finished_order)
                 }
             }
         }
-        pages_in_update[page_idx] = *page_num;        
+        finished_order.push(current_node);
     }
 
-    corrected_update
-    
+    for &node in nodes_to_consider {
+        if !visited.contains(&node) {
+            dfs(node, adj_list, nodes_to_consider, &mut visited, &mut finished_order);
+        }
+    }
+    finished_order
 }
-    */
 
 fn parse_input() -> (HashMap<u32, HashSet<u32>>, Vec<Vec<u32>>) {
     let data = fs::read_to_string("inputs/day5.txt")
         .expect("Input file should be available");
 
-    let mut must_precede: HashMap<u32, HashSet<u32>> = HashMap::new();
+    let mut successors: HashMap<u32, HashSet<u32>> = HashMap::new();
 
     data.lines()
         .take_while(|line| !line.is_empty())
@@ -78,18 +91,17 @@ fn parse_input() -> (HashMap<u32, HashSet<u32>>, Vec<Vec<u32>>) {
             (num1, num2)
         })
         .for_each(|(num1, num2)| {
-            // num1 must precede num2
-            let predecessors = must_precede.entry(num2).or_insert(HashSet::new());
-            predecessors.insert(num1);
+            // num2 must be after num1
+            let successor_list = successors.entry(num1).or_insert(HashSet::new());
+            successor_list.insert(num2);
         });
 
     // Separator that separates rules and updates
     let sep = data.find("\n\n")
         .expect("The input should contain two line breaks separating the rules and updates");
 
-    let page_updates = &data[sep + 2..];
+    let update_data = &data[sep + 2..];
+    let updates = parse_str_slice_to_integers(update_data, ",");
 
-    let updates = parse_str_slice_to_integers(page_updates, ",");
-
-    (must_precede, updates)
+    (successors, updates)
 }
